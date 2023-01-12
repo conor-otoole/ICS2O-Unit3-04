4064db5c4411f8fb2493d235b11e1e9fefcdee83
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2023-01-12 14:13:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: conor-otoole
-// Created on: Sep 2020
+// Created on: Jan 2023
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit3-04/sw.js", {
@@ -16,8 +16,16 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function converts Fahrenheit to Celsius
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function enterClicked() {
+  //input
+  const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+
+  //process
+  const celsius = ((fahrenheit - 32) * 5) / 9
+
+  //output
+  document.getElementById("celsius").innerHTML =
+    "(" + fahrenheit + "℉ - 32) x 5/9 = " + celsius.toFixed(2) + "℃"
 }
